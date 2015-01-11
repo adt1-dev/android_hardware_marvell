350484890522589408f0d302dfded5c85388ae49
c457 <android.c357@gmail.com>
2015-01-10 21:29:50 -0600

    libbt: Make Marvell Wireless Daemon optional

--- libbt/Android.mk
+++ libbt/Android.mk
@@ -13,13 +13,15 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES += \
         $(LOCAL_PATH)/include \
         $(BDROID_DIR)/hci/include \
-        hardware/marvell/wlan/mrvl/libMarvellWireless
 
 LOCAL_SHARED_LIBRARIES := \
-        libcutils \
-        libMarvellWireless
+        libcutils
  
+ifneq ($(MRVL_WIRELESS_DAEMON_API),)
 LOCAL_CFLAGS += -DMRVL_WIRELESS_DAEMON_API
+LOCAL_C_INCLUDES += hardware/marvell/wlan/libMarvellWireless
+LOCAL_SHARED_LIBRARIES += libMarvellWireless
+endif
 
 LOCAL_MODULE := libbt-vendor
 LOCAL_MODULE_TAGS := optional
